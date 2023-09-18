2530d9569e690c516426db7bfcd0a86edc642390
AHsofty <e.basaran2004@gmail.com>
2023-09-19 00:33:01 +0200

    Feat: Created a bottom navigation bar

--- app/src/main/java/com/example/e_reader/MainActivity.java
+++ app/src/main/java/com/example/e_reader/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.e_reader;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import org.jetbrains.annotations.NotNull;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
+    BottomNavigationView bottomNavigationView;
+
+    SearchFragment searchFragment = new SearchFragment();
+    HomeFragment homeFragment = new HomeFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(this.getSupportActionBar()).hide();
+        this.bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        this.bottomNavigationView
+                .setOnItemSelectedListener(this);
+        this.bottomNavigationView.setSelectedItemId(R.id.item_2);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_1:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, this.searchFragment)
+                        .commit();
+                return true;
+
+            case R.id.item_2:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, this.homeFragment)
+                        .commit();
+                return true;
+
+            case R.id.item_3:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, this.settingsFragment)
+                        .commit();
+                return true;
+        }
+        return false;
     }
 }
