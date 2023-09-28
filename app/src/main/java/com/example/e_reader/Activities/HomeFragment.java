e6b267d10a14687c8d8baf55eac1f899e4cf0f33
AHsofty <e.basaran2004@gmail.com>
2023-09-28 20:22:54 +0200

    Feat: Added a feature where the user can now add select a book which will add it to the database

--- app/src/main/java/com/example/e_reader/Activities/HomeFragment.java
+++ app/src/main/java/com/example/e_reader/Activities/HomeFragment.java
@@ -1,63 +1,90 @@
 package com.example.e_reader.Activities;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
+
+import android.widget.Toast;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.example.e_reader.Activities.Database.Book;
-import com.example.e_reader.Activities.Database.BookDao;
-import com.example.e_reader.Activities.Database.BookDatabase;
 import com.example.e_reader.Activities.Database.BookViewModel;
 import com.example.e_reader.R;
 
 import java.util.List;
-import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
     private Uri theBookFileUri;
     private BookViewModel viewModel;
+    private ActivityResultLauncher<Intent> sActivityResultLauncher;
 
-    public HomeFragment(){
+
+    public HomeFragment() {
         // require a empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        sActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Intent data = result.getData();
+                        this.theBookFileUri = data.getData();
+
+                        viewModel.getAllBooks().observe(getViewLifecycleOwner(), books -> {
+                            // Before we add a book to the database we check if the database already contains that book
+                            // We can do this by checking if the URI is already in the database or not
+                            if (books.stream().noneMatch(book -> book.getUri().equals(this.theBookFileUri.toString()))) {
+                                String title = this.theBookFileUri.getLastPathSegment();
+                                Book book = new Book();
+                                book.setUri(this.theBookFileUri.toString());
+                                book.setTitle(title);
+                                this.viewModel.insert(book);
+                            }
+                            else {
+                                Toast.makeText(getContext(), "You have already added the book you selected", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+        );
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_home, container, false);
-        this.viewModel = new ViewModelProvider(this).get(BookViewModel.class);
-
         ImageView importView = rootview.findViewById(R.id.importBtn);
         importView.setOnClickListener(this::openFileDialog);
+
+        // Initialize the ViewModel with the activity's lifecycle
+        this.viewModel = new ViewModelProvider(requireActivity()).get(BookViewModel.class);
+
+        // Observe the LiveData for books
+        viewModel.getAllBooks().observe(getViewLifecycleOwner(), books -> {
+            for (Book book : books) {
+                // TODO: Add logic for a recyclerview with a bunch of cards for each book
+
+            }
+        });
+
         return rootview;
     }
 
-    ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                    Intent data = result.getData();
-                    this.theBookFileUri = data.getData();
-                    Log.d("HomeFragment", "onActivityResult: " + this.theBookFileUri);
-                }
-            }
-    );
+
 
     public void openFileDialog(View view) {
         Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
@@ -65,6 +92,4 @@ public class HomeFragment extends Fragment {
         data = Intent.createChooser(data, "Choose a file");
         sActivityResultLauncher.launch(data);
     }
-
-
 }
